484c89f9f905076938103e281ee7d2f4fc988775
GGTechArt <141692391+GGTechArt@users.noreply.github.com>
2023-08-18 17:47:08 -0500

    Primera configuracion

---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Syetem.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace PolimorfismoCurso
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
+
